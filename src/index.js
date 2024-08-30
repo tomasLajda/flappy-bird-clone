a883b6d95ea3f7694f42deac410411174c3e7b5a
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-30 12:02:18 +0200

    added a score scene

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 import MenuScene from './scenes/MenuScene';
 import PlayScene from './scenes/PlayScene';
 import PreloadScene from './scenes/PreloadScene';
+import ScoreScene from './scenes/ScoreScene';
 
 const WIDTH = 800;
 const HEIGHT = 600;
@@ -13,7 +14,7 @@ const SHARED_CONFIG = {
   startPosition: BIRD_POSITION,
 };
 
-const Scenes = [PreloadScene, MenuScene, PlayScene];
+const Scenes = [PreloadScene, MenuScene, PlayScene, ScoreScene];
 const createScene = (Scene) => new Scene(SHARED_CONFIG);
 const initScenes = () => Scenes.map((Scene) => createScene(Scene));
 

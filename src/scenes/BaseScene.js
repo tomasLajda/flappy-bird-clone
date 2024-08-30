7868070b9d1b7c39186a1a12c1677979a4c4224b
tomas.lajda <tomas.lajda0@gmail.com>
2024-08-30 11:30:38 +0200

    added menu

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -5,10 +5,32 @@ class BaseScene extends Phaser.Scene {
     super(key);
 
     this.config = config;
+    this.screenCenter = [config.width / 2, config.height / 2];
+
+    this.lineHeight = 0;
+    this.fontSize = 32;
+    this.fontOptions = { fontSize: `${this.fontSize}px`, fill: '#CD00FF' };
   }
 
   create() {
     this.add.image(0, 0, 'sky').setOrigin(0);
+  }
+
+  createMenu(menu) {
+    let lastMenuPositionY = 0;
+
+    menu.forEach((menuItem) => {
+      const menuPosition = [
+        this.screenCenter[0],
+        this.screenCenter[1] + this.lineHeight,
+      ];
+
+      this.add
+        .text(...menuPosition, menuItem.text, this.fontOptions)
+        .setOrigin(0.5, 1);
+
+      this.lineHeight += 42;
+    });
   }
 }
 
